908c95931d32c2f5072361e100ef9fb8108bc7f2
puihayyip <puihay.yip@gmail.com>
2022-06-04 16:57:45 +0800

    stats done. Testing with API next

--- my-finance-app/src/components/CompanyDetails/Statistics.jsx
+++ my-finance-app/src/components/CompanyDetails/Statistics.jsx
@@ -163,69 +163,79 @@ function Statistics() {
           <h3>Income statement</h3>
           <div>
             <p>Revenue (ttm) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData?.totalRevenue.fmt}</p>
           </div>
           <div>
             <p>Revenue per share (ttm) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData?.revenuePerShare.fmt}</p>
           </div>
           <div>
             <p>Quarterly revenue growth (yoy) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData?.revenueGrowth.fmt}</p>
           </div>
           <div>
             <p>Gross profit (ttm) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData?.grossProfits.fmt}</p>
           </div>
           <div>
             <p>EBITDA </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData?.ebitda.fmt}</p>
           </div>
           <div>
             <p>Net income avi to common (ttm) </p>
-            <p>94.05B</p>
+            <p>
+              {
+                state.companySummary?.defaultKeyStatistics?.netIncomeToCommon
+                  ?.fmt
+              }
+            </p>
           </div>
           <div>
             <p>Diluted EPS (ttm) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.trailingEps.fmt}</p>
           </div>
           <div>
             <p>Quarterly earnings growth (yoy) </p>
-            <p>94.05B</p>
+            <p>
+              {
+                state.companySummary?.defaultKeyStatistics
+                  .earningsQuarterlyGrowth.fmt
+              }
+            </p>
           </div>
           <h3>Balance sheet</h3>
           <div>
             <p>Total cash (mrq) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData.totalCash.fmt}</p>
           </div>
           <div>
             <p>Total cash per share (mrq) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData.totalCashPerShare.fmt}</p>
           </div>
           <div>
             <p>Total debt (mrq) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData.totalDebt.fmt}</p>
           </div>
           <div>
             <p>Total debt/equity (mrq) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData.debtToEquity.fmt}</p>
           </div>
           <div>
             <p>Current ratio (mrq) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData.currentRatio.fmt}</p>
           </div>
           <div>
             <p>Book value per share (mrq) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics.bookValue.fmt}</p>
           </div>
           <h3>Cash flow statement</h3>
           <div>
             <p>Operating cash flow</p>
-            <p>5.91B</p>
+            <p>{state.companySummary?.financialData.operatingCashflow.fmt}</p>
           </div>
           <div>
             <p>Levered free cash flow (ttm) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.financialData.freeCashflow.fmt}</p>
           </div>
         </div>
 
@@ -234,121 +244,138 @@ function Statistics() {
           <h3>Stock price history</h3>
           <div>
             <p>Beta (5Y monthly) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail.beta.fmt}</p>
           </div>
           <div>
             <p>52-week change</p>
-            <p>94.05B</p>
+            <p>
+              {state.companySummary?.defaultKeyStatistics["52WeekChange"].fmt}
+            </p>
           </div>
           <div>
             <p>S&P500 52-week change</p>
-            <p>94.05B</p>
+            <p>
+              {
+                state.companySummary?.defaultKeyStatistics?.SandP52WeekChange
+                  .fmt
+              }
+            </p>
           </div>
           <div>
             <p>52-week high</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.fiftyTwoWeekHigh.fmt}</p>
           </div>
           <div>
             <p>52-week low</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.fiftyTwoWeekLow.fmt}</p>
           </div>
           <div>
             <p>50-day moving average</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.fiftyDayAverage.fmt}</p>
           </div>
           <div>
             <p>200-day moving average</p>
-            <p>94.05B</p>
+            <p>
+              {state.companySummary?.summaryDetail?.twoHundredDayAverage.fmt}
+            </p>
           </div>
           <h3>Share statistics</h3>
           <div>
             <p>Avg vol (3-month)</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.averageVolume.fmt}</p>
           </div>
           <div>
             <p>Avg vol (10-day)</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.price?.averageDailyVolume10Day.fmt}</p>
           </div>
           <div>
             <p>Shares outstanding </p>
-            <p>94.05B</p>
+            <p>
+              {state.companySummary?.defaultKeyStatistics?.sharesOutstanding.fmt}
+            </p>
           </div>
           <div>
             <p>Implied shares outstanding</p>
-            <p>94.05B</p>
+            <p>
+              {state.companySummary?.defaultKeyStatistics
+                ?.impliedSharesOutstanding.fmt
+                ? state.companySummary?.defaultKeyStatistics
+                    ?.impliedSharesOutstanding.fmt
+                : "N/A"}
+            </p>
           </div>
           <div>
             <p>Float</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.floatShares.fmt}</p>
           </div>
           <div>
             <p>% held by insiders</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.heldPercentInsiders.fmt}</p>
           </div>
           <div>
             <p>% held by institutions</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.heldPercentInstitutions.fmt}</p>
           </div>
           <div>
             <p>Shares short (12 May 2022)</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.sharesShort.fmt}</p>
           </div>
           <div>
             <p>Short ratio (12 May 2022)</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.shortRatio.fmt}</p>
           </div>
           <div>
             <p>Short % of float (12 May 2022) </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.shortPercentOfFloat.fmt}</p>
           </div>
           <div>
             <p>Short % of shares outstanding (12 May 2022)</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.sharesPercentSharesOut.fmt}</p>
           </div>
           <div>
             <p>Shares short (prior month 13 Apr 2022)</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.sharesShortPriorMonth.fmt}</p>
           </div>
           <h3>Dividends & splits</h3>
           <div>
             <p>Forward annual dividend rate </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.dividendRate.fmt}</p>
           </div>
           <div>
             <p>Forward annual dividend yield </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.dividendYield.fmt}</p>
           </div>
           <div>
             <p>Trailing annual dividend rate </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.trailingAnnualDividendRate.fmt}</p>
           </div>
           <div>
             <p>Trailing annual dividend yield</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.trailingAnnualDividendYield.fmt}</p>
           </div>
           <div>
             <p>5-year average dividend yield</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.fiveYearAvgDividendYield.fmt}</p>
           </div>
           <div>
             <p>Payout ratio </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.summaryDetail?.payoutRatio.fmt}</p>
           </div>
           <div>
             <p>Dividend date </p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.calendarEvents?.dividendDate.fmt}</p>
           </div>
           <div>
             <p>Ex-dividend date</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.calendarEvents?.exDividendDate.fmt}</p>
           </div>
           <div>
             <p>Last split factor</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.lastSplitFactor}</p>
           </div>
           <div>
             <p>Last split date</p>
-            <p>94.05B</p>
+            <p>{state.companySummary?.defaultKeyStatistics?.lastSplitDate.fmt}</p>
           </div>
         </div>
       </div>
